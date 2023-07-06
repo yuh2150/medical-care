97bd6598dc42012efb82c3816467eb1bb2afe35d
Stork Nguyen <storknguyen.286@gmail.com>
2023-07-06 21:54:27 +0700

    update format prettier part 2

--- assets/js/chuyenkhoa.js
+++ assets/js/chuyenkhoa.js
@@ -1,45 +1,42 @@
 $(document).ready(function () {
-    $('.nav-links').each(function () {
-      const navSlides = $(this).find('.nav-tabs--slider')
-      const contentSlides = $(this).find('.tab-content--slider')
-  
-      contentSlides.slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        // arrows: true,
-        dots: false,
-        infinite: false,
-        asNavFor: navSlides,
-        // infinite: true,
-        mobileFirst:true,
-        
-        // prevArrow:"<button type='button' class='slick-prev slide-arrow'><i class='fa-solid fa-angles-left' aria-hidden='true'></i></button>",
-        // nextArrow:"<button type='button' class='slick-next slide-arrow'><i class='fa-solid fa-angles-right' aria-hidden='true'></i></button>"
-      });
-  
-      navSlides.slick({
-        slidesToShow: 24,
-        asNavFor: contentSlides,
-        dots: false,
-        arrows: false, 
-        focusOnSelect: true,
-        responsive: [
-          {
-            breakpoint: 767,
-            settings: {
-              slidesToShow: 10,
-            },
+  $(".nav-links").each(function () {
+    const navSlides = $(this).find(".nav-tabs--slider");
+    const contentSlides = $(this).find(".tab-content--slider");
+
+    contentSlides.slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      // arrows: true,
+      dots: false,
+      infinite: false,
+      asNavFor: navSlides,
+      // infinite: true,
+      mobileFirst: true,
+
+      // prevArrow:"<button type='button' class='slick-prev slide-arrow'><i class='fa-solid fa-angles-left' aria-hidden='true'></i></button>",
+      // nextArrow:"<button type='button' class='slick-next slide-arrow'><i class='fa-solid fa-angles-right' aria-hidden='true'></i></button>"
+    });
+
+    navSlides.slick({
+      slidesToShow: 24,
+      asNavFor: contentSlides,
+      dots: false,
+      arrows: false,
+      focusOnSelect: true,
+      responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 10,
           },
-          {
-            breakpoint: 700,
-            settings: {
-              slidesToShow: 6,
-            },
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 6,
           },
+        },
       ],
-      });
-      
     });
   });
-
-
+});
--- assets/js/nhathuoc.js
+++ assets/js/nhathuoc.js
@@ -1,32 +1,34 @@
-$(document).ready(function() {
-    $('.list-card-product').slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        prevArrow:"<button type='button' class='slick-prev slide-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-        nextArrow:"<button type='button' class='slick-next slide-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
-        responsive: [
-            {
-              breakpoint: 1200,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-              },
-            },
-            {
-              breakpoint: 992,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-              },
-            },
-            {
-                breakpoint: 660,
-                settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1,
-                },
-            },
-        ],
-    });
-}); 
+$(document).ready(function () {
+  $(".list-card-product").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      "<button type='button' class='slick-prev slide-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next slide-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 660,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
